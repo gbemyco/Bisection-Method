905cf7d7be358a08ce23fa1aadd330d9f6a0f3f0
Gbemi Abass <105248808+gbemyco@users.noreply.github.com>
2022-09-27 07:11:59 +0100

    Add files via upload

---
+++ Bisection.py
@@ -0,0 +1,32 @@
+def f(x):
+    return x**3-x**2-1
+
+def bisection(a,b,n):
+    i = 1
+    condition = True
+    while condition:
+        x =(a+b)/2
+        if f(x)<0:
+            a=x
+        else:
+            b=x
+        print('iteration = ',i, 'x = ',x, 'f(x) = ', f(x))
+        if i == n:
+            condition = False
+        else:
+            condition = True
+            i = i + 1
+    print('Required root is : ', x)
+
+a = input ('First approximation root : ')
+b = input ('Second approximation root : ')
+n = input ('No. of iteration : ')
+a = float(a)
+b = float(b)
+n = int(n)
+
+if f(a)*f(b)>0:
+    print('Given approximate root do not bracket the root.')
+    print('Try again with different values.')
+else:
+    bisection(a,b,n)
